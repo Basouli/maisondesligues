59eaee90c88746ebba37b5c8d5ac9d841c26ddc9
Basouli <74357835+Basouli@users.noreply.github.com>
2022-03-30 15:12:08 +0200

    first ok commit

--- var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
+++ var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
@@ -10,7 +10,7 @@ if (in_array(PHP_SAPI, ['cli', 'phpdbg'], true)) {
 }
 
 require dirname(__DIR__, 3).''.\DIRECTORY_SEPARATOR.'vendor/autoload.php';
-require __DIR__.'/ContainerDzYeI9O/srcApp_KernelDevDebugContainer.php';
+require __DIR__.'/ContainerVxUEQnk/srcApp_KernelDevDebugContainer.php';
 
 $classes = [];
 $classes[] = 'Symfony\Bundle\FrameworkBundle\FrameworkBundle';

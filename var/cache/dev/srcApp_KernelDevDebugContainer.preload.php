32343899d47b39c045d3fdad0d7705cbc73416ff
Basouli <74357835+Basouli@users.noreply.github.com>
2022-04-25 11:38:18 +0200

    25/04/2022

--- var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
+++ var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
@@ -10,7 +10,7 @@ if (in_array(PHP_SAPI, ['cli', 'phpdbg'], true)) {
 }
 
 require dirname(__DIR__, 3).''.\DIRECTORY_SEPARATOR.'vendor/autoload.php';
-require __DIR__.'/ContainerZg4CLZp/srcApp_KernelDevDebugContainer.php';
+require __DIR__.'/Container8xZrlxg/srcApp_KernelDevDebugContainer.php';
 
 $classes = [];
 $classes[] = 'Symfony\Bundle\FrameworkBundle\FrameworkBundle';
@@ -28,6 +28,7 @@ $classes[] = 'Symfony\Bundle\FrameworkBundle\Controller\ControllerNameParser';
 $classes[] = 'Symfony\Bundle\FrameworkBundle\EventListener\ResolveControllerNameSubscriber';
 $classes[] = 'Symfony\Component\DependencyInjection\ServiceLocator';
 $classes[] = 'App\Controller\MainController';
+$classes[] = 'App\Repository\HotelRepository';
 $classes[] = 'Doctrine\Bundle\DoctrineBundle\Controller\ProfilerController';
 $classes[] = 'Doctrine\Bundle\DoctrineBundle\Dbal\ManagerRegistryAwareConnectionProvider';
 $classes[] = 'Doctrine\Bundle\DoctrineBundle\Registry';

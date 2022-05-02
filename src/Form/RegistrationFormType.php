4968eede01374a1a2a225f2dc405f9671ccbebd1
Basouli <74357835+Basouli@users.noreply.github.com>
2022-05-02 17:22:02 +0200

    register account

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Compte;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -80,7 +80,7 @@ class RegistrationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Compte::class,
+            'data_class' => User::class,
         ]);
     }
 }

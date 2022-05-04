b6627a18c27c1f8472259a53fec4a99b3c59aa9d
Basouli <74357835+Basouli@users.noreply.github.com>
2022-05-04 17:13:02 +0200

    hfdh

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Repository\LicencieRepository;
+use App\Repository\UserRepository;
 
 class RegistrationController extends AbstractController
 {
